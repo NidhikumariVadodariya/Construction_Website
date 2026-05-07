2b742e80b217c37b245a864d61ee8d4d4ceebbea
NidhikumariVadodariya <NV29@myscc.ca>
2026-05-07 15:13:06 -0400

    Initial portfolio project

---
+++ script.js
@@ -0,0 +1,143 @@
+// Javascript
+
+// Open / close mobile menu
+
+function toggleMenu () {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+
+}
+
+// Get navbar links, menu, and button
+
+// const navLinks = document.getElementById("navLinks");
+// const menuBtn = document.querySelector(".menu-btn");
+// const navItems = document.querySelectorAll(".nav-links a");
+// const sections = document.querySelectorAll("section");
+
+//const revealElements = document.querySelectorAll(".reveal");
+
+
+// Close mobile menu when clicking any menu link or inside area of menu box
+
+const navItems = document.querySelectorAll(".nav-links a");
+const navLinks = document.getElementById("navLinks");
+
+navItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+    });
+});
+
+// Close mobile menu when clicking outside of the website
+
+// const navLinks = document.getElementById("navLinks");
+const menuBtn = document.querySelector(".menu-btn");
+
+document.addEventListener("click", function (event) {
+    if (!navLinks.contains(event.target) && !menuBtn.contains(event.target)) {
+        navLinks.classList.remove("active");
+    }
+});
+
+// Highlight active menu item while scrolling
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", function () {
+    let currentSection = "";
+
+    sections.forEach(function (section) {
+        const sectionTop = section.offsetTop - 130;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            currentSection = section.getAttribute("id");
+        }
+    });
+
+    navItems.forEach(function (item) {
+        item.classList.remove("active");
+
+        if(item.getAttribute("href") === "#" + currentSection) {
+            item.classList.add("active");
+        }
+    });
+});
+
+// What will happen
+// Section starts hidden
+// When you scroll near it
+// JavaScript adds active
+// CSS changes opacity to 1 and moves it up smoothly
+
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll () {
+    revealElements.forEach(function (element) {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if 
+        (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
+
+
+
+
+
+
+
+// ***********************
+// ***********************
+// this code is clean without used any more attributes comments... 
+
+
+// function toggleMenu() {
+//   const navLinks = document.getElementById("navLinks");
+//   navLinks.classList.toggle("active");
+// }
+
+// const navLinks = document.getElementById("navLinks");
+// const menuBtn = document.querySelector(".menu-btn");
+// const navItems = document.querySelectorAll(".nav-links a");
+// const sections = document.querySelectorAll("section");
+
+// navItems.forEach(function (item) {
+//   item.addEventListener("click", function () {
+//     navLinks.classList.remove("active");
+//   });
+// });
+
+// document.addEventListener("click", function (event) {
+//   if (!navLinks.contains(event.target) && !menuBtn.contains(event.target)) {
+//     navLinks.classList.remove("active");
+//   }
+// });
+
+// window.addEventListener("scroll", function () {
+//   let currentSection = "";
+
+//   sections.forEach(function (section) {
+//     const sectionTop = section.offsetTop - 130;
+//     const sectionHeight = section.offsetHeight;
+
+//     if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+//       currentSection = section.getAttribute("id");
+//     }
+//   });
+
+//   navItems.forEach(function (item) {
+//     item.classList.remove("active");
+
+//     if (item.getAttribute("href") === "#" + currentSection) {
+//       item.classList.add("active");
+//     }
+//   });
+// });
